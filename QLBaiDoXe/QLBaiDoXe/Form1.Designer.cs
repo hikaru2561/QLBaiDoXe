cd092d30d2224a1e88dcc22ba8b5e1d2aa59fafe
hikaru2561 <nguyenkimquang2561@gmail.com>
2025-04-09 22:31:31 +0700

    Tạo bảng + class Booking

--- QLBaiDoXe/QLBaiDoXe/Form1.Designer.cs
+++ QLBaiDoXe/QLBaiDoXe/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(357, 277);
+            this.button2.Location = new System.Drawing.Point(456, 174);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
