2dcf6a850ccd15b4315bcd0912b27d89d2790391
hikaru2561 <nguyenkimquang2561@gmail.com>
2025-04-09 22:35:50 +0700

    Viết Unit test

--- QLBaiDoXe/QLBaiDoXe/Form1.Designer.cs
+++ QLBaiDoXe/QLBaiDoXe/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(541, 257);
+            this.button2.Location = new System.Drawing.Point(550, 184);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
