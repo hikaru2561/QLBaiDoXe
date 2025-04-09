e6d67a9adbe326058d1b4f4a9fa8c79d38977b4e
hikaru2561 <nguyenkimquang2561@gmail.com>
2025-04-09 22:47:13 +0700

    Viết controller xử lý việc hủy

--- QLBaiDoXe/QLBaiDoXe/Form1.Designer.cs
+++ QLBaiDoXe/QLBaiDoXe/Form1.Designer.cs
@@ -65,7 +65,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(195, 323);
+            this.checkBox1.Location = new System.Drawing.Point(144, 243);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 3;
